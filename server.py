4df31889849c15da9dcec0a383023a38eee93f4f
Ben Miller <ben@ben-server.home>
2017-08-16 21:25:06 -0400

    add city selector -- still need to make functional; adjusted layout of display; pass city options to display

--- server.py
+++ server.py
@@ -2,17 +2,31 @@ from flask import Flask
 from flask import render_template
 import sqlalchemy as sql
 import pandas as pd
-import os
+import json
 app = Flask(__name__)
 
 
-@app.route('/')
-def index(city='New York City'):
+@app.route('/city/<int:cid>')
+def index(cid):
+        # connect to sql backend
         engine = sql.create_engine('postgresql://localhost:5432/govex')
-        query = "SELECT * FROM city_wiki WHERE city = '{}';".format(city)
+        
+        # get city ids
+        query = "SELECT city FROM city_wiki;"
+        cities = pd.read_sql_query(query, engine)['city']
+        
+        query = "SELECT * \
+                 FROM city_wiki \
+                 WHERE city = '{}';".format(cities[cid])
         city_data = pd.read_sql_query(query, engine)
+        
         return render_template('index.html',
-                               title='title',
-                               city=city,
+                               title='City-Wiki',
+                               city=cities[cid],
                                mayor=city_data['mayor'][0],
-                               img=city_data['img'][0])
+                               gtype=city_data['type'][0],
+                               img=city_data['img'][0],
+                               website=city_data['website'][0],
+                               cities=cities)
+
+
